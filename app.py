2fa777bda9d9bba606d771e7679964ef3ec75b22
Dave Johnson <dajohnso@redhat.com>
2023-03-22 12:17:47 -0400

    change 4-bad

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! Change #3b"
+    return "Hello World! Change #4-bad
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
